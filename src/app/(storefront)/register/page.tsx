d8c24c45d3fec9818ec2479349634186dfce85b1
bhomik-glitch <awebsite601@gmail.com>
2026-04-20 22:24:59 +0530

    background bug resolved

--- src/app/(storefront)/register/page.tsx
+++ src/app/(storefront)/register/page.tsx
@@ -6,7 +6,7 @@ export default function RegisterPage() {
         <main className="min-h-screen flex items-center justify-center px-4 pt-24 relative overflow-hidden">
 
             <div className="absolute inset-0 -z-10">
-                <div className="w-full h-full bg-[url('/images/form-bg-content.png')] bg-cover bg-center" />
+                <div className="w-full h-full bg-[url('/images/Form-bg-content.png')] bg-cover bg-center" />
                 <div className="absolute inset-0 bg-black/30" />
             </div>
 
